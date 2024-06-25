5de6154c201e351c3bd55a3cb1b77c36462be546
Thanus Kumaar <thanuskumaara@gmail.com>
2024-06-25 22:00:23 +0530

    Modified frontend to send data to add tasks into the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,63 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
+  const [timeToConsume, setTime] = useState();
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function addToFile() {
+    try {
+      const response = await invoke("add_task_to_file", {
+        name: name,
+        date: date,
+        category: category,
+        completionTime: parseInt(timeToConsume, 10),
+      });
+      console.log(response);
+    } catch (error) {
+      console.error("Error invoking command:", error);
+    }
   }
 
   return (
-    <div className="container">
+    <div className="bg-black">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
-        className="row"
+        className="flex flex-col w-[60%] mx-auto gap-3"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          addToFile();
         }}
       >
         <input
-          id="greet-input"
+        className="p-2 rounded-md"
+          id="task-name"
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          placeholder="Task Name"
         />
-        <button type="submit">Greet</button>
+        <input
+        className="p-2 rounded-md"
+          id="task-cat"
+          onChange={(e) => setCategory(e.currentTarget.value)}
+          placeholder="Category"
+        />
+        <input
+        className="p-2 rounded-md"
+          id="due-date"
+          onChange={(e) => setDate(e.currentTarget.value)}
+          placeholder="Due Date"
+        />
+        <input
+        className="p-2 rounded-md"
+          id="task-comp-time"
+          onChange={(e) => setTime(e.currentTarget.value)}
+          placeholder="Time to complete (hrs)"
+        />
+        <button className="bg-white p-3 w-28 text-center mx-auto rounded-md" type="submit">Submit</button>
       </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
