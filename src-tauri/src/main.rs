b4bd3db8868827e1b7ca247912ab770e2a929282
Thanus Kumaar <thanuskumaara@gmail.com>
2024-06-26 15:34:50 +0530

    Implemented heap data structure using vectors and added state management feature of application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,22 +9,35 @@ use std::path::Path;
 static FILE_PATH: &str = "C:/Users/Tanushkumaaar/OneDrive/Desktop/Tasks.txt";
 
 // struct defining the task
-struct Task<'a> {
-    _name: &'a str,
-    _date: &'a str,
-    _category: &'a str,
+#[derive(Debug)]
+struct Task {
+    _name: String,
+    _date: String,
+    _category: String,
     _completion_time: u32,
 }
 
-// command to add data into file
+// command to add data into file and heap
 #[tauri::command]
 fn add_task_to_file(
-    name: &str,
-    date: &str,
-    category: &str,
+    name: String,
+    date: String,
+    category: String,
     completion_time: u32,
+    state: tauri::State<'_, AppState>,
 ) -> Result<String, ()> {
-    println!("HELLO");
+    let task: Task = Task {
+        _name: name.clone(),
+        _date: date.clone(),
+        _category: category.clone(),
+        _completion_time: completion_time,
+    };
+    
+    // Push task to heap
+    let mut heap = state.heap.lock().unwrap();
+    push_heap(&mut heap, task)?;
+
+    // Write to file
     let string_to_write = format!("{},{},{},{}\n", name, date, category, completion_time);
     let mut file = File::options()
         .append(true)
@@ -32,54 +45,48 @@ fn add_task_to_file(
         .expect("Unable to open file for writing");
     file.write_all(string_to_write.as_bytes())
         .expect("Error writing into file!");
+
+    print_heap(&heap);
+
     Ok(String::from("All Good"))
 }
 
-// Other functions
 // Function to add data into the heap
-fn push_heap<'a>(heap: &mut Vec<Option<Box<Task<'a>>>>, task: Task<'a>) -> Result<i32, ()> {
+fn push_heap(heap: &mut Vec<Option<Box<Task>>>, task: Task) -> Result<(), ()> {
     heap.push(Some(Box::new(task)));
-    Ok(200)
+    Ok(())
+}
+
+// Function to print heap
+fn print_heap(heap: &Vec<Option<Box<Task>>>) {
+    for i in heap {
+        if let Some(v) = i {
+            println!("{}", v._name);
+        }
+    }
+}
+
+// State struct to hold the heap
+struct AppState {
+    heap: std::sync::Mutex<Vec<Option<Box<Task>>>>, // Using Mutex for thread safety
 }
 
 // main function that runs the application loop
 fn main() {
+    // Create file if it doesn't exist
     let path = Path::new(FILE_PATH);
-    // create file if it doesn't exist
     if !path.exists() {
         File::create(path).expect("Unable to create file");
     }
-    // creating heap data structure
-    let mut heap: Vec<Option<Box<Task>>> = Vec::new();
-    let task: Task = Task {
-        _name: "asd",
-        _date: "1/1/1",
-        _category: "qwe",
-        _completion_time: 12,
-    };
-    push_heap(&mut heap, task).expect("Error");
-    let task: Task = Task {
-        _name: "asd",
-        _date: "1/1/1",
-        _category: "qwe",
-        _completion_time: 12,
-    };
-    push_heap(&mut heap, task).expect("Error");
-    let task: Task = Task {
-        _name: "asd",
-        _date: "1/1/1",
-        _category: "qwe",
-        _completion_time: 12,
-    };
-    push_heap(&mut heap, task).expect("Error");
 
-    for i in &heap{
-        if let Some(v) = i{
-            println!("{}", v._name);
-        }
-    }
+    // Initialize the heap
+    let heap: Vec<Option<Box<Task>>> = Vec::new();
+    let app_state = AppState {
+        heap: std::sync::Mutex::new(heap),
+    };
 
     tauri::Builder::default()
+        .manage(app_state) // Manage app state
         .invoke_handler(tauri::generate_handler![add_task_to_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
